e88b73cd38b32c3beca2ac1038deb1bda4fa1b2b
Zahirasafa <raraadiah6@gmail.com>
2024-10-07 18:56:21 +0700

    Update ProjectDDP2.cpp

--- ProjectDDP2.cpp
+++ ProjectDDP2.cpp
@@ -3,4 +3,18 @@
 using namespace std;
 
 void tampilkanMenu(){
-  cout << "=== Checking the Area and Perimeter of 2-Dimensional Flat Figures in meters
+  cout << "=== Checking the Area and Perimeter of 2-Dimensional Flat Figures in meters ===" << endl;
+  cout << "=== Project by Group 2, Mata Kuliah Dasar-Dasar Pemrograman ===" << endl;
+  cout << "1. Persegi" << endl;
+  cout << "2. Persegi Panjang" << endl;
+  cout << "3. Lingkaran" << endl;
+  cout << "4. Segitiga" << endl;
+  cout << "5. Trapesium" << endl;
+  cout << "6. Jajar Genjang" << endl;
+  cout << "7. Belah Ketupat" << endl;
+  cout << "8. Layang-layang" << endl;
+  cout << "9. Selesai" << endl;
+}
+
+void hitungBangunan (int pilihan){
+  double sisi, panjang, lebar, jariJari, alas ,tinggi, sisiMiring, sisi1, sisi2, diagonal1, diagonal2, luas, keliling; 
